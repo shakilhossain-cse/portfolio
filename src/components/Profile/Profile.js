1e56811c93fd516fb7b98eb0adbad1f732e0d150
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-11-29 23:02:08 +0600

    edit alt in profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -9,7 +9,7 @@ const Profile = () => {
           <div className={styles.photo}>
             <img
               src="https://scontent.fdac14-1.fna.fbcdn.net/v/t39.30808-6/240462304_567782011328591_9115769117975561781_n.jpg?_nc_cat=105&_nc_rgb565=1&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeG9bx7BQ_XFFLndqCjSyWDxu4IBrw_5wyy7ggGvD_nDLMe4mlE9xgWH-gwHCrnQL1qjoF4jud55NQ2qm3U1JmYx&_nc_ohc=jfE-I3V303YAX-IlGGI&tn=f6pF-vacycQDVdUU&_nc_ht=scontent.fdac14-1.fna&oh=067899be4c15fd2dae33ab6e9bf651b2&oe=61AA3E46"
-              alt="profile photo"
+              alt="profile_photo"
             />
           </div>
           <div className={styles.data}>
