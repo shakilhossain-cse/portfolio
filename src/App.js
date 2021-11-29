55dc6779f289ac9e34fe02c89dbaf5f72e27d4d8
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-11-29 20:22:43 +0600

    remove logo import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    Hello Wrold
-    </div>
-  );
+  return <div className="App">Hello Wrold</div>;
 }
 
 export default App;
