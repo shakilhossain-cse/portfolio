a7143777bf45e9a24a5b778804f70655040219bd
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-11-30 20:20:41 +0600

    filter added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Contact from "./Contact/Contact";
-// import About from "./About/About";
+import About from "./About/About";
 import styles from "./Main.module.css";
 import Sidebar from "./Sidebar/Sidebar";
-// import Works from "./Works/Works";
+import Works from "./Works/Works";
+import Blog from "./Blog/Blog";
 
 const Main = () => {
+  const [category, setCategory] = useState("about");
   return (
     <div className={styles.container}>
-      <Sidebar />
+      <Sidebar category={category} setCategory={setCategory} />
       <div className={styles.content}>
-        <Contact />
+        {category == "about" ? (
+          <About />
+        ) : category == "works" ? (
+          <Works />
+        ) : category == "contact" ? (
+          <Contact />
+        ) : category == "blog" ? (
+          <Blog />
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
--- src/components/Main/Sidebar/Sidebar.js
+++ src/components/Main/Sidebar/Sidebar.js
@@ -1,14 +1,22 @@
 import React from "react";
 import styles from "./Sidebar.module.css";
 
-const Sidebar = () => {
+const Sidebar = ({ category, setCategory }) => {
   return (
     <div className={styles.sidebar_container}>
       <div className={styles.sidebar} style={{ background: "#fff" }}>
-        <div className={styles.button}>ABOUT</div>
-        <div className={styles.button}>WORKS</div>
-        <div className={styles.button}>BLOGS</div>
-        <div className={styles.button}>CONTACT</div>
+        <div className={styles.button} onClick={() => setCategory("about")}>
+          ABOUT
+        </div>
+        <div className={styles.button} onClick={() => setCategory("works")}>
+          WORKS
+        </div>
+        <div className={styles.button} onClick={() => setCategory("blog")}>
+          BLOGS
+        </div>
+        <div className={styles.button} onClick={() => setCategory("contact")}>
+          CONTACT
+        </div>
       </div>
     </div>
   );
