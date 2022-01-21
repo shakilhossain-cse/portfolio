8b5dd3dab58648f95cfdb8f6c4e913c6d9518137
shakilhossain-cse <shakilhossainp.cse@gmail.com>
2022-01-21 22:08:22 +0600

    home page done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { Route, Switch } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import AboutPage from "./components/AboutPage"
+import BlogPage from "./components/BlogPage"
+import Main from "./components/Main"
+import MySkillPage from "./components/MySkillPage"
+import NotFoundpage from "./components/NotFoundpage"
+import { lightTheme } from "./components/Theme"
+import WorkPage from "./components/WorkPage"
+import GlobalStyle from "./globalStyles"
+
 function App() {
-  return <div>
-    App File
-    </div>
-    
+  return <>
+    <GlobalStyle />
+    <ThemeProvider theme={lightTheme}>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/about" component={AboutPage} />
+        <Route exact path="/blog" component={BlogPage} />
+        <Route exact path="/work" component={WorkPage} />
+        <Route exact path="/skills" component={MySkillPage} />
+        <Route exact path="/*" component={NotFoundpage} />
+      </Switch>
+    </ThemeProvider>
+  </>
+
 }
 
 export default App
---
+++ src/components/MySkillPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MySkillPage = () => {
+  return <div>my skill page</div>;
+};
+
+export default MySkillPage;
