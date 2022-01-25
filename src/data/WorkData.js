730f22e81063e85182ccf2e9207a4bce522970d1
shakilhossain-cse <shakilhossainp.cse@gmail.com>
2022-01-25 11:28:17 +0600

    added my project

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,61 +1,30 @@
 export const Work = [
     {
         id:1,
-        name:"Agency Landing Page",
-        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
-        tags:["react","gsap","styledComponents"],
-        
-        demo:"https://agency-website-eta.vercel.app/",
-        github:"https://github.com/codebucks27/Agency-website"
+        name:"Watch Website - WrishShopWatch Website",
+        description:"Admin Login info email : admin@admin.com , password: 123456",
+        tags:["react","react-bootstrap","firebase","nodejs"],
+        image:"https://i.ibb.co/FzFg5y9/Screenshot-from-2022-01-25-11-13-43.png",
+        demo:"https://wrishshop.web.app/",
+        github:"https://github.com/shakilhossain-cse/wrishshop_client"
     },
     {
         id:2,
-        name:"Job listing website",
-        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
-        tags:["react","sass"],
-        
-        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/react-job-listing-website"
+        name:"Tourism Website - PaxosTurJob listing website",
+        description:"This website is for those people who like to travel.",
+        tags:["react","react-bootstrap","firebase"],
+        image:"https://i.ibb.co/qNsHWK9/Screenshot-from-2022-01-25-11-18-58.png",
+        demo:"https://paxostur.web.app/",
+        github:"https://github.com/shakilhossain-cse/paxoxtur_client"
         
     },
     {
         id:3,
-        name:"Dev.to Clone",
-        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
-        tags:["react","api","sass"],
-        
-        demo:"http://devto-clone-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/devto-clone"
-    },
-    {
-        id:4,
-        name:"Sidebar Menu",
-        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
-        tags:["react","styledComponents","react-router"],
-        
-        demo:"https://react-sidebar.vercel.app/",
-        github:"https://github.com/codebucks27/react-sidebar"
-       
-    },{
-        id:5,
-        name:"Todo App(React + Redux)",
-        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
-        tags:["react","redux","framer-motion"],
-        
-        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-Redux-Todo-App"
-        
-    },
-    {
-        id:6,
-        name:"Responsive Navigation Menu",
-        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-        tags:["react","react-router","css"],
-        
-        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-responsive-navbar"
-        
-    },
-    
-  
+        name:"Health-Care Website - MedService",
+        description:"It's a healthcare related website.",
+        tags:["react","react-bootstrap","firebase"],
+        image:"https://i.ibb.co/QPLZ58n/Screenshot-from-2022-01-25-11-21-15.png",
+        demo:"https://medservice-57a0c.web.app/",
+        github:"https://github.com/shakilhossain-cse/health_care"
+    }
 ] 
